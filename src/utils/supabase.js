e670be275ef7367b2fc92a13dd3baeb0e70089bf
miskaslzickou <ilikemoonm@gmail.com>
2025-11-03 21:30:32 +0100

    supabase fix

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Vytvoření instance je mimo funkci, aby se stalo jen jednou
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_KEY;
+const supabaseAnonKey = process.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export const useSupabase = () => {
